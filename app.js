205f242432de04922cb1f3afd4b340ee1269190f
SylvainLangler <sylvain.langler@etu.unice.fr>
2019-04-13 22:19:28 +0200

    ajout partie cv non finie

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ $(document).ready(function () {
   $(".menu2 a").click(function(){
       $(".menu2").slideToggle();
       var target = $(this).attr("href");
-      $('html, body').stop().animate({scrollTop: $(target).offset().top}, 500 ); 
+      $('html, body').stop().animate({scrollTop: $(target).offset().top}, 700 ); 
   });
 
   $(".menu a").click(function(){
       var target = $(this).attr("href");
-      $('html, body').stop().animate({scrollTop: $(target).offset().top}, 500 ); 
+      $('html, body').stop().animate({scrollTop: $(target).offset().top}, 700 ); 
   });
 
   /* Animations tri portfolio */
