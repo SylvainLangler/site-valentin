28fa868468cc3313092afa5b5610af67060e003e
SylvainLangler <sylvain.langler@etu.unice.fr>
2019-04-16 01:00:40 +0200

    ajout images partie skills + anim + ptits réglages left/right arrow responsive

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ $(document).ready(function () {
       if($(".hamburger").hasClass("active")){
         TweenMax.to(".hamburger img",0.3,{x:0, ease:SteppedEase.config(8)});
         $(".hamburger").removeClass("active");
+        $("body").css("overflow", "initial");
       }
       else{
         TweenMax.to(".hamburger img",0.3,{x:-400, ease:SteppedEase.config(8)});
         $(".hamburger").addClass("active");
+        $("body").css("overflow", "hidden");
       }
       $(".menu2").slideToggle();
   });
@@ -115,11 +117,22 @@ $(document).ready(function () {
 
   // ANIMATION SKILLS //
 
+  $(".images_skills img").hide();
+
   // au clic sur une des catégories, on demande à animer l'image correspondante
   $(".anim img").bind("click", function(){
     var selected = $(this).parent().attr('id');
     animate(selected);
+    $(".images_skills img").hide();
+    showImage(selected);
   });
+
+  $(".anim .title").bind("click",function(){
+    var selected = $(this).parent().siblings().children().attr('id');
+    animate(selected);
+    $(".images_skills img").hide();
+    showImage(selected)
+  })
 
 
   
@@ -138,6 +151,14 @@ $(document).ready(function () {
       TweenMax.to('#'+selected+' img',0.3,{x:-520, ease:SteppedEase.config(8)});
       TweenMax.to('#'+previousAnimated+' img',0.3,{x:0, ease:SteppedEase.config(8)});
       previousAnimated = selected;
+    }
+  }
+
+  function showImage(selected){
+    var img = "img_"+selected;
+    $("#"+img).fadeIn(300);
+    if(window.innerWidth < 769){
+      $('html, body').stop().animate({scrollTop: $(".images_skills").offset().top}, 1000 );
     }
   }
 
